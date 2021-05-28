e472043c20711cf8a5e9f8b27be67a18df3e89d4
Antoine Brossas <antoine.brossas.etu@univ-lille.fr>
2021-05-28 15:11:31 +0200

    Update requetes.h : requete 1

--- includes/requetes.h
+++ includes/requetes.h
@@ -1,10 +1,42 @@
+#include "data.h"
+
 #define MAX_DATE 5
 
-//#include "data.h" // fonctions en rapport avec nos structures de données (déclaration, load..)
+//----------- STRUCTURES DE DONNEES POUR LES REQUETES -----------//
 
-void show_airports (char *, Liste_airports);
+ struct cell_IATA // on utilise cette structure de données pour stocker uniquement les codes IATA des aéroports pour la requête 1 (show-airport)
+{
+	char airport[IATA_AIRPORT_MAX];
+	struct cell_IATA *p_next;
+};
 
-void show_airlines (char *, Liste_airlines);
+typedef struct cell_IATA * Liste_IATA;
+
+
+typedef struct Date // on utilise cette structure pour stocker des dates de départ
+{
+    int month;
+    int day;
+    int weekday;
+} Date;
+
+//----------- FONCTIONS POUR LES REQUETES -----------//
+
+// REQUETE 1 : show-airport
+
+int airport_already_in_list(Liste_IATA, char*);
+
+void show_airports (char*, Liste_airports, Liste_flights);
+
+void add_head(Liste_IATA*, char*);
+
+void show_airports(char*, Liste_airports, Liste_flights);
+
+// REQUETE 2 : show-airline
+
+int flight_already_in_list(Liste_IATA l_iata, Liste_flights l_flights);
+
+void show_airlines (char*, Liste_airlines);
 
 void show_flights (char *, char); // optionnel : heure de début et nombre limites de vol à afficher
 
